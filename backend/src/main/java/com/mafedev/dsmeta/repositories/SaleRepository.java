683fbf8c9d214fb0b82b3147d2e0127cbd51631e
eaemafe <maria.kbst_17@hotmail.com>
2022-07-13 23:13:13 -0300

    API test

---
+++ backend/src/main/java/com/mafedev/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.mafedev.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mafedev.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/com/mafedev/dsmeta/services/SaleService.java
@@ -0,0 +1,20 @@
+package com.mafedev.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mafedev.dsmeta.entities.Sale;
+import com.mafedev.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+}
